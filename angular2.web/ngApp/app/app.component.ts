1bf8cc60bf6e13b968028384f5f6a94ff52877d5
innercitypressure <caseysmithokcity@gmail.com>
2016-06-02 21:17:33 -0500

    adding styles, about to convert to tour of heros tutorial app

--- angular2.web/ngApp/app/app.component.ts
+++ angular2.web/ngApp/app/app.component.ts
@@ -2,6 +2,6 @@
 
 @Component({
     selector: 'my-app',
-    template: `<h1>My angular app with .ts actually compiling.</h1>`
+    template: `<h1>My angular app with .ts actually compiling. checking for changes..!</h1>`
 })
 export class AppComponent { }
